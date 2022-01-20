70997479f63b75f8cddc2ef1419ff059ac3efa7e
Sean Moriarity <smoriarity.5@gmail.com>
2022-01-19 20:36:27 -0500

    Start adding tests

---
+++ lib/tpuserve/model_config.ex
@@ -0,0 +1,99 @@
+defmodule TPUServe.ModelConfig do
+  @moduledoc """
+  Model configuration.
+  """
+  alias __MODULE__, as: ModelConfig
+
+  @enforce_keys [:name, :inputs, :outputs]
+  defstruct :name, :inputs, :outputs
+
+  def parse!(config) do
+    config
+    |> Jason.decode!()
+    |> normalize!()
+    |> then(&struct(ModelConfig, &1))
+  end
+
+  # Get config into format accepted by model manager,
+  # and do as much validation as possible so we don't
+  # screw up the NIF
+  defp normalize!(config) do
+    %{name: name, inputs: inputs, outputs: outputs} = config
+
+    name = normalize_name!(name)
+    inputs =
+      inputs
+      |> Enum.map(&normalize_tensor_spec!/1)
+      |> ensure_unique_names!()
+
+    outputs =
+      outputs
+      |> Enum.map(&normalize_tensor_spec!/1)
+      |> ensure_unique_names!()
+
+    %{config |
+      name: name,
+      inputs: inputs,
+      outputs: outputs}
+  end
+
+  # Name represents the key in the model manager and
+  # as a consequence needs to be acceptable as a URL
+  # endpoint - TODO
+  defp normalize_name(name) do
+    name
+  end
+
+  # Tensor spec is a name, a shape, and a type. We need
+  # to validate the shape to ensure all dimensions are
+  # strictly positive and the type to ensure it is an
+  # acceptable TPU type
+  defp normalize_tensor_spec(tensor_spec) do
+    %{name: name, shape: shape, type: type} = tensor_spec
+
+    # TODO: name
+    shape = normalize_shape!(name, shape)
+    type = normalize_type!(name, type)
+
+    %{name: name, shape: shape, type: type}
+  end
+
+  defp normalize_shape!(name, shape) when is_list(shape) do
+    shape
+    |> Enum.map(& &1 > 0)
+    |> Enum.all?()
+  end
+
+  defp normalize_shape!(name, shape) do
+    # TODO: Custom Error
+    raise ArgumentError, "inavlid shape for tensor spec #{inspect(name)}:" <>
+                            " #{inspect(shape)}"
+  end
+
+  defp normalize_type(name, type) when is_binary(type) do
+    case type do
+      "BF16" -> {:bf, 16}
+      "F32" -> {:f, 32}
+      "S32" -> {:s, 32}
+    end
+  end
+
+  defp normalize_type(name, type) do
+    # TODO: Custom Error
+    raise ArgumentError, "invalid type for tensor spec #{inspect(name)}:" <>
+                            " #{inspect(type)}"
+  end
+
+  defp ensure_unique_names!(tensor_specs) do
+    are_all_unique? =
+      tensor_specs
+      |> Enum.map(& &1.name)
+      |> then(&Enum.count(&1) == Enum.count(Enum.uniq(&1)))
+
+    unless are_all_unique? do
+      raise ArgumentError, "tensor spec names must all be unique"
+    end
+
+    tensor_specs
+  end
+end
---
+++ test/tpuserve/model_config_test.exs
@@ -0,0 +1,101 @@
+defmodule TPUServe.ModelConfigTest do
+  use ExUnit.Case
+
+  alias TPUServe.ModelConfig
+
+  describe "parse!" do
+    resnet_config = """\
+    {
+      "name": "ResNet50",
+      "inputs": [
+        {
+          "name": "image",
+          "shape": [1, 224, 224, 3],
+          "type": "F32"
+        }
+      ],
+      "outputs": [
+        {
+          "name": "class",
+          "shape": [1, 1000],
+          "type": "F32"
+        }
+      ]
+    }
+    """
+
+    test "resnet config" do
+      assert config = %ModelConfig{} = ModelConfig.parse!(resnet_config)
+      assert [image] = config.inputs
+      assert [class] = config.outputs
+      assert image.name == "image"
+      assert image.shape == [1, 224, 224, 3]
+      assert image.type == {:f, 32}
+      assert class.name == "class"
+      assert class.shape == [1, 1000]
+      assert class.type == {:f, 32}
+    end
+
+    duplicate_input_names = """\
+    {
+      "name": "ResNet50",
+      "inputs": [
+        {
+          "name": "image",
+          "shape": [1, 224, 224, 3],
+          "type": "F32"
+        },
+        {
+          "name": "image",
+          "shape": [1, 223, 223, 3],
+          "type": "F32"
+        }
+      ],
+      "outputs": [
+        {
+          "name": "class",
+          "shape": [1, 1000],
+          "type": "F32"
+        }
+      ]
+    }
+    """
+
+    test "raises on duplicate input names" do
+      assert_raise ArgumentError, ~r/tensor specs/, fn ->
+        ModelConfig.parse!(duplicate_input_names)
+      end
+    end
+
+    duplicate_output_names = """\
+    {
+      "name": "ResNet50",
+      "inputs": [
+        {
+          "name": "image",
+          "shape": [1, 224, 224, 3],
+          "type": "F32"
+        }
+      ],
+      "outputs": [
+        {
+          "name": "class",
+          "shape": [1, 1000],
+          "type": "F32"
+        },
+        {
+          "name": "class",
+          "shape": [2, 1],
+          "type": "F32"
+        }
+      ]
+    }
+    """
+
+    test "raises on duplicate output names" do
+      assert_raise ArgumentError, ~r/tensor spec/, fn ->
+        ModelConfig.parse!(duplicate_output_names)
+      end
+    end
+  end
+end
